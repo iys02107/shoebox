932211e6972dc8c0204b591ae6628091c986c4d8
ingyu <ingyu3879@gmail.com>
2022-05-31 06:27:30 +0900

    프로젝트 생성

---
+++ src/test/java/kr/co/shoebox/ShoeboxApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.shoebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
