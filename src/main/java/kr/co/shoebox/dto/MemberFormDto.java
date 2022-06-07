c633ef1105b300b1235a9ec4bdc3e05ecd17bfcd
ingyu <ingyu3879@gmail.com>
2022-06-08 00:44:54 +0900

    layout 수정

--- src/main/java/kr/co/shoebox/dto/MemberFormDto.java
+++ src/main/java/kr/co/shoebox/dto/MemberFormDto.java
@@ -14,18 +14,18 @@ public class MemberFormDto {
     @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
 
-    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private String email;
 
-    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     @Length(min=8, max=16, message = "비밀번호는 8자 이상, 16자 이하로 입력해주세요")
     private String password;
 
-    @NotEmpty(message="비밀번호를 한번 더 입력하세요")
+    @NotBlank(message="비밀번호를 한번 더 입력하세요")
     private String passwordCheck;
 
-    @NotEmpty(message = "우편번호는 필수 입력 값입니다.")
+    @NotBlank(message = "우편번호는 필수 입력 값입니다.")
     private String postcode;
 
     @NotEmpty(message = "도로명 주소는 필수 입력 값입니다.")
@@ -34,6 +34,7 @@ public class MemberFormDto {
     @NotEmpty(message = "상세주소는 필수 입력 값입니다.")
     private String detailAddress;
 
-    @NotEmpty(message = "휴대폰 번호는 필수 입력 값입니다.")
+    @NotBlank(message = "휴대폰 번호는 필수 입력 값입니다.")
+    @Length(min= 10, max=11, message = "올바른 전화번호를 입력하세요")
     private String phoneNumber;
 }
